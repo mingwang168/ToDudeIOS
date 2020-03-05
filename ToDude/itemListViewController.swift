2404453b78bb95249695f2e31e3b7f89f4d2a9e0
Ming <mingwang0904@gmail.com>
2020-03-05 13:15:51 -0800

    3

--- ToDude/itemListViewController.swift
+++ ToDude/itemListViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 import CoreData
+import SwipeCellKit
 
-class itemListViewController: UITableViewController {
+class itemListViewController: UITableViewController, SwipeTableViewCellDelegate {
+   
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  
   var items = [Item]()
-    override func viewDidLoad() {
+  
+  @IBAction func addItemButtonTapped(_ sender: UIBarButtonItem) {
+    var tempTextField = UITextField()
+    
+    let alertController = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+    let alertAction = UIAlertAction(title: "Done", style: .default) {(action) in
+      let newItem = Item(context: self.context)
+      if let text = tempTextField.text {
+        newItem.title = text
+        newItem.completed = false
+        self.items.append(newItem)
+        self.saveItems()
+      }
+    }
+    
+    alertController.addTextField{(textField) in
+      textField.placeholder = "Title"
+      tempTextField = textField
+    }
+    
+    alertController.addAction(alertAction)
+    
+    present(alertController, animated: true, completion: nil)
+  }
+  
+
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    tableView.rowHeight = 80.0
+        loadItems()
     }
 
     // MARK: - Table view data source
@@ -29,7 +56,8 @@ class itemListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as! SwipeTableViewCell
+      cell.delegate = self
 
         // Configure the cell...
         let item = items[indexPath.row]
@@ -41,5 +69,35 @@ class itemListViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let item = items[indexPath.row]
     item.completed = !item.completed
+    saveItems()
+  }
+  func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+    guard orientation == .right else {return nil}
+    let deleteAction = SwipeAction(style: .destructive, title: "Delete"){_, indexPath in
+      self.context.delete(self.items[indexPath.row])
+      self.items.remove(at: indexPath.row)
+      self.saveItems()
+    }
+    deleteAction.image = UIImage(named: "trash")
+    return [deleteAction]
+  }
+  
+  func saveItems(){
+    do {
+      try context.save()
+    }catch{
+      print("Error saving context \(error)")
+    }
+    tableView.reloadData()
+  }
+  
+  func loadItems(){
+    let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+    do{
+      items = try context.fetch(fetchRequest)
+    }catch{
+      print("Error fetching items: \(error)")
+    }
+    tableView.reloadData()
   }
 }
